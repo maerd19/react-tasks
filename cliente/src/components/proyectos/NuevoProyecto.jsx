b074182bbd7fab559f9f2884259397e5be589f0d
maerd19 <revolutions_333@hotmail.com>
2020-09-13 22:44:12 -0500

    Agregando la function para mostrar el formulario

--- cliente/src/components/proyectos/NuevoProyecto.jsx
+++ cliente/src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import proyectoContext from "./../../context/proyectos/proyectoContext";
 
 const NuevoProyecto = () => {
+  // Obtener el state del formulario
+  const proyectosContext = useContext(proyectoContext);
+  const { formulario, mostrarFormulario } = proyectosContext;
+
   // State para el proyecto
   const [proyecto, setProyecto] = useState({
     nombre: "",
@@ -33,28 +38,39 @@ const NuevoProyecto = () => {
     });
   };
 
+  // Mostrar el formulario
+  const onClickFormulario = () => {
+    mostrarFormulario();
+  };
+
   return (
     <>
-      <button type="button" className="btn btn-block btn-primario">
+      <button
+        type="button"
+        className="btn btn-block btn-primario"
+        onClick={onClickFormulario}
+      >
         Nuevo Proyecto
       </button>
 
-      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-          value={nombre}
-          onChange={onChangeProyecto}
-        />
+      {formulario ? (
+        <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre Proyecto"
+            name="nombre"
+            value={nombre}
+            onChange={onChangeProyecto}
+          />
 
-        <input
-          type="submit"
-          value="Agregar Proyecto"
-          className="btn btn-primario btn-block"
-        />
-      </form>
+          <input
+            type="submit"
+            value="Agregar Proyecto"
+            className="btn btn-primario btn-block"
+          />
+        </form>
+      ) : null}
     </>
   );
 };
