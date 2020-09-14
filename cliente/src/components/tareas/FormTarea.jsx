870db93244d17a629d5152f7c348243710b4bf06
maerd19 <revolutions_333@hotmail.com>
2020-09-14 13:46:05 -0500

    Ocultando el formulario de tareas

--- cliente/src/components/tareas/FormTarea.jsx
+++ cliente/src/components/tareas/FormTarea.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import proyectoContext from "./../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+  // Extraer si un proyecto esta activo
+  const { proyecto } = useContext(proyectoContext);
+
+  // Si no hay proyecto seleccionado
+  if (!proyecto) return null;
+
   return (
     <div className="formulario">
       <form>
