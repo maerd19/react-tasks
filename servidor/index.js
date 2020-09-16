5ab40aea1839c9c22888e9cdd0ad2b5a2f647b50
maerd19 <revolutions_333@hotmail.com>
2020-09-16 09:09:06 -0500

    Conectando app a Mongo

--- servidor/index.js
+++ servidor/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const conectarDB = require("./config/db");
 
 // Crear el servidor
 const app = express();
+
+// Conectar a la base de datos
+conectarDB();
 
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
