821acf88b67574f77e341a30fd2393acea8d7755
maerd19 <revolutions_333@hotmail.com>
2020-09-16 10:38:02 -0500

    Validacion de usuario unico

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,8 +1,17 @@
 const Usuario = require("./../models/Usuarios");
 
 exports.crearUsuario = async (req, res) => {
+  // extraer email y password
+  const { email, password } = req.body;
+
   try {
-    let usuario;
+    // Revisar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario)
+      return res.status(400).json({
+        msg: "El usuario ya existe",
+      });
 
     // Crear nuevo usuario
     usuario = new Usuario(req.body);
@@ -11,7 +20,9 @@ exports.crearUsuario = async (req, res) => {
     await usuario.save();
 
     // Mensaje de confirmacion
-    res.status(201).send("Usuario creado correctamente");
+    res.status(201).json({
+      msg: "Usuario creado correctamente",
+    });
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
