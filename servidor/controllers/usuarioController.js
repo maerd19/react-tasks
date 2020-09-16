7a4666f126ae6d3f3666d6633ed0cbda799edb30
maerd19 <revolutions_333@hotmail.com>
2020-09-16 10:33:51 -0500

    Insertando usuarios en la BD

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,3 +1,19 @@
-exports.crearUsuario = (req, res) => {
-  console.log(req.body);
+const Usuario = require("./../models/Usuarios");
+
+exports.crearUsuario = async (req, res) => {
+  try {
+    let usuario;
+
+    // Crear nuevo usuario
+    usuario = new Usuario(req.body);
+
+    // Guardar usuario
+    await usuario.save();
+
+    // Mensaje de confirmacion
+    res.status(201).send("Usuario creado correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Hubo un error");
+  }
 };
