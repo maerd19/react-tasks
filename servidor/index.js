031febbfb37f8a07934a356cbb9249072563586e
maerd19 <revolutions_333@hotmail.com>
2020-09-16 09:42:41 -0500

    Configurando rutas de usuarios

--- servidor/index.js
+++ servidor/index.js
@@ -15,6 +15,9 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
 
+// Importar rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
+
 // Arranque de la app
 app.listen(PORT, () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`);
---
+++ servidor/routes/usuarios.js
@@ -0,0 +1,9 @@
+// rutas para crear usuarios
+const express = require("express");
+const router = express.Router();
+
+// Crea un usuario
+// api/usuarios
+router.post("/", () => {});
+
+module.exports = router;
